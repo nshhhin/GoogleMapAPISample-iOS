8345e81cbda793785e36a8d1df5c414e96f822c9
nshhhin <shinn070110@gmail.com>
2019-09-14 19:47:40 +0900

    :tada: プロジェクトファイル作成

---
+++ GoogleMapAPISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMapAPISample
+//
+//  Created by 新納真次郎 on 2019/09/14.
+//  Copyright © 2019 新納真次郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
